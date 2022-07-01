7bc965b0c8e8efa14e049466bd6c39e16c6105d2
Rafael Langoni <langoninaves@gmail.com>
2022-07-01 18:52:41 -0300

    adicionando  comportamentos nos botões

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,24 @@
+let valorDoInput = document.querySelector("#tarefa").value;
+let listaDeItens = document.querySelector(".lista__tarefas");
+let itemDaLista = document.querySelector(".lista__itens")
 let botaoAdicionar = document.querySelector(".botao__adicionar");
+let botaoRemover = document.querySelector(".botao__remover")
+
 botaoAdicionar.addEventListener("click", () =>{
-    alert("teste");
+    if(valorDoInput == '' || valorDoInput == null){
+        alert("Digite alguma tarefa a ser inserida.");
+        return
+    }
+    let itemLista = document.createElement("li");
+    itemLista.appendChild(document.createTextNode(valorDoInput));
+    itemLista.classList.add("lista__itens");
+    listaDeItens.appendChild(itemLista);
+    document.querySelector("#tarefa").value = '';
+})
+
+botaoRemover.addEventListener("click", () =>{
+    if(valorDoInput == '' || valorDoInput == null){
+        alert("Digite alguma tarefa a ser excluída.");
+        return;
+    }
 })
