014f47a5a3c6df194d00072f4306234e2b09cb2e
Rafael Langoni <langoninaves@gmail.com>
2022-07-01 19:54:58 -0300

    funcionalidade de cirar li a partir do clique no botao

--- assets/index.js
+++ assets/index.js
@@ -1,23 +1,36 @@
-let valorDoInput = document.querySelector("#tarefa").value;
 let listaDeItens = document.querySelector(".lista__tarefas");
 let itemDaLista = document.querySelector(".lista__itens")
 let botaoAdicionar = document.querySelector(".botao__adicionar");
 let botaoRemover = document.querySelector(".botao__remover")
 
-botaoAdicionar.addEventListener("click", () =>{
-    if(valorDoInput == '' || valorDoInput == null){
-        alert("Digite alguma tarefa a ser inserida.");
-        return
+botaoAdicionar.addEventListener("click", () => {
+    let valorDoInput = document.querySelector("#tarefa").value;
+    if (valorDoInput == '' || valorDoInput == null) {
+        alert("Digite alguma tarefa a ser incluída.");
+        return;
     }
-    let itemLista = document.createElement("li");
-    itemLista.appendChild(document.createTextNode(valorDoInput));
-    itemLista.classList.add("lista__itens");
-    listaDeItens.appendChild(itemLista);
+    let itemDaListaLi = document.createElement("li");
+    itemDaListaLi.appendChild(document.createTextNode(valorDoInput));
+    itemDaListaLi.classList.add("lista__itens");
+    listaDeItens.appendChild(itemDaListaLi);
     document.querySelector("#tarefa").value = '';
+    let botaoIncluirClicado = true;
+    return botaoIncluirClicado;
 })
 
-botaoRemover.addEventListener("click", () =>{
-    if(valorDoInput == '' || valorDoInput == null){
+/**
+ * essa parte de baixo não está fucional
+ * quero habilitar o clique no item para poder removê-lo
+ * verificar o local onde usar o itemDaLista
+ */
+console.log(itemDaLista)
+itemDaLista.addEventListener("click", () => {
+    itemDaLista.style.backgroundColor = "white";
+})
+
+
+botaoRemover.addEventListener("click", () => {
+    if (valorDoInput == '' || valorDoInput == null) {
         alert("Digite alguma tarefa a ser excluída.");
         return;
     }
