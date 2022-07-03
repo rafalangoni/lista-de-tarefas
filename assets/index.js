6d1b68e21bb41fd718d74bf0b6d2f4db660c4052
Rafael Langoni <langoninaves@gmail.com>
2022-07-03 08:25:18 -0300

    item clicado habilita remoção pelo botão

--- assets/index.js
+++ assets/index.js
@@ -2,7 +2,12 @@ let listaDeItens = document.querySelector(".lista__tarefas");
 let itemDaLista = document.querySelector(".lista__itens")
 let botaoAdicionar = document.querySelector(".botao__adicionar");
 let botaoRemover = document.querySelector(".botao__remover")
+let itemClicado = false;
 
+/**
+ * A variável itemClicado serve para fazer com que se saiba se o item foi clicado ou não para remover.
+ * Se ele foi clicado, o alerta de campo preenchido não deve aparecer, pois o item já existe.
+ */
 
 /**
  * O primeiro Li criado está oculto. Fiz isso porque na primeira adição, como não havia elementos
@@ -25,17 +30,27 @@ botaoAdicionar.addEventListener("click", (evento) => {
 
 botaoRemover.addEventListener("click", () => {
     let valorDoInput = document.querySelector("#tarefa").value;
-    if (valorDoInput == '' || valorDoInput == null) {
-        alert("Digite alguma tarefa a ser excluída.");
-        return;
+    if (itemClicado == false) {
+        if (valorDoInput == '' || valorDoInput == null) {
+            alert("Digite alguma tarefa a ser excluída.");
+            return;
+        }
     }
     let item = document.querySelectorAll("ul .lista__itens");
-    item.forEach( (elemento) => {
-        if(valorDoInput === elemento.textContent){
+    item.forEach((elemento) => {
+        if (valorDoInput === elemento.textContent) {
             listaDeItens.removeChild(elemento);
             document.querySelector("#tarefa").value = '';
         }
     })
 })
 
-console.log("teste")
+listaDeItens.addEventListener("click", (evento) => {
+    itemClicado = true;
+    evento.target.classList.add("item__selecionado");
+    botaoRemover.addEventListener("click", (e) => {
+        document.querySelector("#tarefa").value = evento.target.textContent;
+        evento.originalTarget.hidden = "true";
+        document.querySelector("#tarefa").value = '';
+    })
+})
